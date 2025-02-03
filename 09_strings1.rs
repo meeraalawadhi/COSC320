8961527924b9ecab210bc1c8c8ba08c650c91e28
meeraalawadhi <100058524@ku.ac.ae>
2025-02-03 23:26:41 +0400

    Delete 09_strings1.rs

--- 09_strings1.rs
+++
@@ -1,9 +0,0 @@
-// Fix: Convert the &str to a String
-fn current_favorite_color() -> String {
-    "blue".to_string() // or String::from("blue")
-}
-
-fn main() {
-    let answer = current_favorite_color();
-    println!("My current favorite color is {answer}");
-}
