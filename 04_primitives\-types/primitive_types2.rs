d0d7a4eed0d3b5946e2882985e929e856e1605d6
meeraalawadhi <100058524@ku.ac.ae>
2025-02-03 19:48:16 +0400

    Update primitive_types2.rs

--- 04_primitives\-types/primitive_types2.rs
+++ 04_primitives\-types/primitive_types2.rs
@@ -1,6 +1,5 @@
 fn main() {
-    // Note the _single_ quotes, these are different from the double quotes
-    // you've been seeing around.
+  
     let my_first_initial = 'C';
     if my_first_initial.is_alphabetic() {
         println!("Alphabetical!");
