f19e56c268457bf9bc47fa1c6e7efe4b6315f82a
meeraalawadhi <100058524@ku.ac.ae>
2025-02-03 19:41:02 +0400

    Create primitive_types2.rs

---
+++ 04_primitives\-types/primitive_types2.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Note the _single_ quotes, these are different from the double quotes
+    // you've been seeing around.
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+
+    // Declare `your_character` with a character of your choice
+    let your_character = '😊'; 
+
+    if your_character.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if your_character.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+}
