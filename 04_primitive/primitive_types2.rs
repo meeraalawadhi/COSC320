a5c5a0bae1454aeeb2eca82bcf417c4441596c0f
meeraalawadhi <100058524@ku.ac.ae>
2025-02-03 23:16:06 +0400

    Create primitive_types2.rs

---
+++ 04_primitive/primitive_types2.rs
@@ -0,0 +1,22 @@
+fn main() {
+  
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+
+    // Declare `your_character` with a character of your choice
+    let your_character = '😊'; 
+
+    if your_character.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if your_character.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+}
