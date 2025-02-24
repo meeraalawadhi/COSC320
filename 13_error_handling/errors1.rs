c7efca5fda1f7f5de613f7e5eb599bc057c7eeb5
meeraalawadhi <100058524@ku.ac.ae>
2025-02-24 09:17:16 +0400

    Create errors1.rs

---
+++ 13_error_handling/errors1.rs
@@ -0,0 +1,34 @@
+// Function now returns `Result<String, String>` to handle errors properly.
+fn generate_nametag_text(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        // Return an error message instead of `None`
+        Err("Empty names aren't allowed".to_string())
+    } else {
+        Ok(format!("Hi! My name is {name}"))
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".to_string()),
+            Ok("Hi! My name is Beyoncé".to_string()),
+        );
+    }
+
+    #[test]
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text(String::new()),
+            Err("Empty names aren't allowed".to_string()),
+        );
+    }
+}
