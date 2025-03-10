5125ed9f2479cc6f08c032a826a7aea468ab06c4
meeraalawadhi <100058524@ku.ac.ae>
2025-03-10 09:12:39 +0400

    Create traits2.rs

---
+++ 15_traits/traits2.rs
@@ -0,0 +1,27 @@
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+// Implement `AppendBar` for `Vec<String>`
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar")); // Push "Bar" to the vector
+        self
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), "Bar");
+        assert_eq!(foo.pop().unwrap(), "Foo");
+    }
+}
