b449db7ea1bc03f779300cb9e50d85052583cef1
meeraalawadhi <100058524@ku.ac.ae>
2025-03-17 09:13:38 +0400

    Create lifetimes1.rs

---
+++ 16_lifetimes/lifetimes1.rs
@@ -0,0 +1,20 @@
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    //    ^^^^     ^^          ^^          ^^
+    if x.len() > y.len() { x } else { y }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_longest() {
+        assert_eq!(longest("abcd", "123"), "abcd");
+        assert_eq!(longest("abc", "1234"), "1234");
+    }
+}
