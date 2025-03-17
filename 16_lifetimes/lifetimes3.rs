ad97521958444e29ef6f81f6087839ea81987b69
meeraalawadhi <100058524@ku.ac.ae>
2025-03-17 09:16:05 +0400

    Create lifetimes3.rs

---
+++ 16_lifetimes/lifetimes3.rs
@@ -0,0 +1,18 @@
+
+
+struct Book<'a> {
+    //     ^^^^ added a lifetime annotation
+    author: &'a str,
+    //       ^^
+    title: &'a str,
+    //      ^^
+}
+
+fn main() {
+    let book = Book {
+        author: "George Orwell",
+        title: "1984",
+    };
+
+    println!("{} by {}", book.title, book.author);
+}
