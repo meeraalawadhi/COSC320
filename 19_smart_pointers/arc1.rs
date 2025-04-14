e68cc3663dfcf959dee27a16bdfee4ec83be19a3
meeraalawadhi <100058524@ku.ac.ae>
2025-04-14 09:41:30 +0400

    Create arc1.rs

---
+++ 19_smart_pointers/arc1.rs
@@ -0,0 +1,30 @@
+
+
+// Don't change the lines below.
+#![forbid(unused_imports)]
+use std::{sync::Arc, thread};
+
+fn main() {
+    let numbers: Vec<_> = (0..100u32).collect();
+
+    let shared_numbers = Arc::new(numbers);
+    //                   ^^^^^^^^^^^^^^^^^
+
+    let mut join_handles = Vec::new();
+
+    for offset in 0..8 {
+        let child_numbers = Arc::clone(&shared_numbers);
+        //                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+        let handle = thread::spawn(move || {
+            let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
+            println!("Sum of offset {offset} is {sum}");
+        });
+
+        join_handles.push(handle);
+    }
+
+    for handle in join_handles.into_iter() {
+        handle.join().unwrap();
+    }
+}
